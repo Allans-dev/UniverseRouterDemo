0555115b00853b6b841170e7308afc1dbecb698d
Allan.W.C <allan_195@hotmail.com>
2017-09-04 12:02:19 +1000

    added home menu
     - edited png images in photoshop to get icons
     - colour design
     - formatting layouts

--- src/components/homeMenu.js
+++ src/components/homeMenu.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 const HomeMenu = () => {
     return (
-        <div>
+        <div className="homeMenu">
             <div>
+                <img alt="earth-and-moon-icon" src="imgs/earth-and-moon.png"></img> 
                 Neighborhood<br/>
                 (less than 50 parsecs)
             </div>
             <div>
+                <img alt="solar-system-icon" src="imgs/solar-system.png"></img> 
                 Regional<br/>
                 (less than 200 parsecs)
             </div>
             <div>
+                <img alt="galaxy-icon" src="imgs/galaxy-view.png"></img> 
                 All
             </div>
         </div>
