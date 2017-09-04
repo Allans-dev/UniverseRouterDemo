62721396cff18b2764ae751f21f0adbff75a6cc2
Allan.W.C <allan_195@hotmail.com>
2017-09-04 16:39:16 +1000

    Footer work
     - social media
     - navigation for alternate links
     - footer notes including credit to authors

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className="footer">
+            <div className="row">
+                
+                <div className="footer-social">
+                    <ul>
+                        <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i className="fa fa-google-plus"></i></a></li>
+                        
+                    </ul>
+                </div>
+                <div className="footer-nav">
+                    <ul>
+                        <li><a href="#">Advertise with us</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Legal</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </div>
+                
+                <div className="footer-notes">
+                    Space icons by Freepik.com.
+                    Website created by Allan Cheung.
+                </div>
+                
+            </div>
         </div>
     );
 }
