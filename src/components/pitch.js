bf3a08fad993aab34550247fd3b53379ca036542
Allan.W.C <allan_195@hotmail.com>
2017-09-07 13:08:12 +1000

    Attempt at implementing typer on virtual DOM
     - unsuccessful

--- src/components/pitch.js
+++ src/components/pitch.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import WayPoint from 'react-waypoint';
+import typer from 'typer-js';
 
 const Pitch = () => {
+    
+//    let trigger = 0;
+//    
+//    function updateTrigger () {
+//        trigger = 1;
+//    }
+    
+//    function triggerTyper () {
+//        if (trigger === 1){
+//            return ''
+//        }
+//    }
+    
+    typer('.pitch')
+      .line('This function roolz.')
+      .back(5)
+      .continue('ules!')
+            
     
     return (
         <div className="row">
             <div className="pitch">
-                <div>
-                
-                </div>
+                    
             </div>
         </div>
         
